df736b7d05a352f47e414ec8020f9152235f99ff
hadi0o <hadi.deh19@gmail.com>
2024-07-25 00:04:06 +0330

    home working and footer and header added and working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./core/header/header.component";
+import {FooterComponent} from "./core/footer/footer.component";
+import {HomeComponent} from "./modules/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
