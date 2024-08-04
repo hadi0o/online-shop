8c9deef024ccc0f27fa842c8689da7c5883e7c9f
hadi0o <hadi.deh19@gmail.com>
2024-08-04 23:03:11 +0330

    added routing and some configuration for home template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component";
+import {AboutComponent} from "./modules/about/about.component";
+import {CommentsComponent} from "./modules/comments/comments.component";
+import {ContactComponent} from "./modules/contact/contact.component";
+import {ProductsComponent} from "./modules/products/products.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path: '', component: HomeComponent},
+   {path: 'about', component: AboutComponent},
+   {path: 'comments', component: CommentsComponent},
+   {path: 'contact', component: ContactComponent},
+   {path: 'products', component: ProductsComponent},
+
+    {path: '**', redirectTo: 'not-found'}
+];
